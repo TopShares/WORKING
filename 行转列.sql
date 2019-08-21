8c80b51499cef94d5a2e0e1edd664125891f75ea
liub1922 <liub1922@gmail.com>
2019-08-21 08:40:51 +0800

    .

--- 行转列.sql
+++ 行转列.sql
@@ -29,3 +29,15 @@ SELECT * FROM table pivot( MAX(分数) FOR 课程 IN (语文,数学,物理))a
 
 张三        74          83          93
 */
+*
+
+
+PIVOT函数将列[WEEK]的行值转换为列，并使用聚合函数Count(TotalPrice)来统计每一个Week列在转换前有多少行数据，语句如下所示：
+
+select *
+from ShoppingCart as C 
+PIVOT(count(TotalPrice) FOR [PackNum] IN([*20],[*20H],[*40],[*40H])) AS T 
+
+
+select * from table1
+pivot (一个函数，sum/max/min/count都可以（列显示的值） FOR  XX要显示成列名的那一列  IN ([对应的是XX里的值],[],[],……))
